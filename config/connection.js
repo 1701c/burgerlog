e4983c38af3755cb7b2dac2a291ce03bf95a46f3
1701c <alex@reventlow.net>
2019-01-28 17:51:58 -0600

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,11 @@
 var mysql = require("mysql");
 
+
 var connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-  console.log(JAWSDB_URL);
+  console.log(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
     port: 3306,
